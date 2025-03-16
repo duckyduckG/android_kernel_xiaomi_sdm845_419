b06298a4fe2dcc1f448cb91cf5416b6ff774f382
duckyduckg <duckyduckg65@gmail.com>
2025-03-16 11:43:30 +0500

    drivers: thermal: Import missing xiaomi thermal changes * change to mi_drm*
    Signed-off-by: duckyduckg <duckyduckg65@gmail.com>

--- drivers/thermal/thermal_core.c
+++ drivers/thermal/thermal_core.c
@@ -29,6 +29,10 @@
 #include "thermal_core.h"
 #include "thermal_hwmon.h"
 
+#ifdef CONFIG_DRM
+#include <drm/drm_notifier_mi.h>
+#endif
+
 MODULE_AUTHOR("Zhang Rui");
 MODULE_DESCRIPTION("Generic thermal management sysfs support");
 MODULE_LICENSE("GPL v2");
@@ -52,6 +56,15 @@ static bool power_off_triggered;
 static struct thermal_governor *def_governor;
 
 static struct workqueue_struct *thermal_passive_wq;
+
+#ifdef CONFIG_DRM
+struct screen_monitor {
+	struct notifier_block thermal_notifier;
+	int screen_state; /* 1: on; 0:off */
+};
+
+struct screen_monitor sm;
+#endif
 
 static atomic_t switch_mode = ATOMIC_INIT(-1);
 static atomic_t temp_state = ATOMIC_INIT(0);
@@ -1681,6 +1694,18 @@ thermal_message_of_batt_show(struct device *dev,
 static DEVICE_ATTR(batt_message, 0644,
 		   thermal_message_of_batt_show, NULL);
 
+#ifdef CONFIG_DRM
+static ssize_t
+thermal_screen_state_show(struct device *dev,
+				      struct device_attribute *attr, char *buf)
+{
+	return snprintf(buf, PAGE_SIZE, "%d\n", sm.screen_state);
+}
+
+static DEVICE_ATTR(screen_state, 0644,
+		   thermal_screen_state_show, NULL);
+#endif
+
 static ssize_t
 thermal_sconfig_show(struct device *dev,
 				      struct device_attribute *attr, char *buf)
@@ -1757,6 +1782,11 @@ static int create_thermal_message_node(void) {
 		ret = sysfs_create_file(&thermal_message_dev.kobj, &dev_attr_batt_message.attr);
 		if (ret < 0)
 			pr_warn("Thermal: create batt message node failed\n");
+#ifdef CONFIG_DRM
+		ret = sysfs_create_file(&thermal_message_dev.kobj, &dev_attr_screen_state.attr);
+		if (ret < 0)
+			pr_warn("Thermal: create batt message node failed\n");
+#endif
 		ret = sysfs_create_file(&thermal_message_dev.kobj, &dev_attr_sconfig.attr);
 		if (ret < 0)
 			pr_warn("Thermal: create sconfig node failed\n");
@@ -1777,9 +1807,43 @@ static void destroy_thermal_message_node(void) {
 	sysfs_remove_file(&thermal_message_dev.kobj, &dev_attr_temp_state.attr);
 	sysfs_remove_file(&thermal_message_dev.kobj, &dev_attr_sconfig.attr);
 	sysfs_remove_file(&thermal_message_dev.kobj, &dev_attr_boost.attr);
+#ifdef CONFIG_DRM
+	sysfs_remove_file(&thermal_message_dev.kobj, &dev_attr_screen_state.attr);
+#endif
 	sysfs_remove_file(&thermal_message_dev.kobj, &dev_attr_batt_message.attr);
 	device_unregister(&thermal_message_dev);
 }
+
+#ifdef CONFIG_DRM
+static int screen_state_for_thermal_callback(struct notifier_block *nb, unsigned long val, void *data)
+{
+	struct mi_drm_notifier *evdata = data;
+	unsigned int blank;
+
+	if (val != MI_DRM_EVENT_BLANK || !tm || !evdata || !evdata->data)
+		return 0;
+
+	blank = *(int *)(evdata->data);
+	switch (blank) {
+	case MI_DRM_BLANK_LP1:
+		pr_warn("%s: MI_DRM_BLANK_LP1\n", __func__);
+	case MI_DRM_BLANK_POWERDOWN:
+		sm.screen_state = 0;
+		pr_warn("%s: MI_DRM_BLANK_POWERDOWN\n", __func__);
+		break;
+	case MI_DRM_BLANK_UNBLANK:
+		sm.screen_state = 1;
+		pr_warn("%s: MI_DRM_BLANK_UNBLANK\n", __func__);
+		break;
+	default:
+		break;
+	}
+
+	sysfs_notify(&thermal_message_dev.kobj, NULL, "screen_state");
+
+	return NOTIFY_OK;
+}
+#endif
 
 static int __init thermal_init(void)
 {
@@ -1822,6 +1886,13 @@ static int __init thermal_init(void)
 		pr_warn("Thermal: create thermal message node failed, return %d\n",
 			result);
 
+#ifdef CONFIG_DRM
+	sm.thermal_notifier.notifier_call = screen_state_for_thermal_callback;
+	if (mi_drm_register_client(&sm.thermal_notifier) < 0) {
+		pr_warn("Thermal: register screen state callback failed\n");
+	}
+#endif
+
 	return 0;
 
 exit_zone_parse:
@@ -1841,6 +1912,9 @@ error:
 
 static void thermal_exit(void)
 {
+#ifdef CONFIG_DRM
+	mi_drm_unregister_client(&sm.thermal_notifier);
+#endif
 	free_thermal_message();
 	unregister_pm_notifier(&thermal_pm_nb);
 	of_thermal_destroy_zones();
