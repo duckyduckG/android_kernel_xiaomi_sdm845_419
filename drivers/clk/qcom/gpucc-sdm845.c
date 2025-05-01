c3837475c46d816bdae0efc98500c335a69f6363
duckyduckg <duckyduckg65@gmail.com>
2025-05-02 00:56:22 +0500

    clk: qcom: sdm845: specify sync probes
    Signed-off-by: duckyduckg <duckyduckg65@gmail.com>

--- drivers/clk/qcom/gpucc-sdm845.c
+++ drivers/clk/qcom/gpucc-sdm845.c
@@ -828,6 +828,7 @@ static struct platform_driver gpu_cc_sdm845_driver = {
 	.driver = {
 		.name = "gpu_cc-sdm845",
 		.of_match_table = gpu_cc_sdm845_match_table,
+		.probe_type = PROBE_FORCE_SYNCHRONOUS,
 	},
 };
 
