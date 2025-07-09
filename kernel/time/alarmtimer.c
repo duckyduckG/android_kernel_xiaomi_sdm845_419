115d05580b4aba2669e52d9005b20e12c5dc7a61
duckyduckg <duckyduckg65@gmail.com>
2025-07-09 09:00:46 +0500

    kernel: time: alarmtimer: revert useless changes
    reason: delay in notifications and wakeups.
    
    reverted changes:
    7b60c2a7e536 kernel/time: do not fail suspend
    1878ae5bac83 Reapply "alarmtimer: Minimize wakeup time"
    
    some changes are kept from commit:
    7b60c2a7e536 kernel/time: do not fail suspend
    
    Signed-off-by: duckyduckg <duckyduckg65@gmail.com>

--- kernel/time/alarmtimer.c
+++ kernel/time/alarmtimer.c
@@ -290,7 +290,7 @@ static int alarmtimer_suspend(struct device *dev)
 		return 0;
 
 	if (ktime_to_ns(min) < 2 * NSEC_PER_SEC)
-		pm_wakeup_event(dev, ktime_to_ms(min) + 10);
+		pm_wakeup_event(dev, 2 * MSEC_PER_SEC);
 
 	trace_alarmtimer_suspend(expires, type);
 
@@ -303,7 +303,7 @@ static int alarmtimer_suspend(struct device *dev)
 	/* Set alarm, if in the past reject suspend briefly to handle */
 	ret = rtc_timer_start(rtc, &rtctimer, now, 0);
 	if (ret < 0)
-		pm_wakeup_event(dev, ktime_to_ms(min) + 10);
+		pm_wakeup_event(dev, MSEC_PER_SEC);
 	return ret;
 }
 
