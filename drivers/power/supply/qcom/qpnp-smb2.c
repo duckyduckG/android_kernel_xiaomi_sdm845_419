Tashar02/kernel_sdm660_tempest-4.4

drivers: power: expose property rerun apsd status
* fixed logspam when charging or usb connected:
  [  119.070339] get prop 152 is not supported in usb

Change-Id: I3bd243667029795cd148191867c80b9a6b403b48

[@Tashar02: For my case it's the following logspam
[    1.074309] get prop 153 is not supported in usb ]

Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

drivers/power/supply/qcom/qpnp-smb2.c
@@ -736,6 +736,11 @@ static int smb2_usb_get_prop(struct power_supply *psy,
 		val->intval = get_client_vote(chg->usb_icl_votable,
 					      USB_PSY_VOTER);
 		break;
+#ifdef CONFIG_MACH_XIAOMI_SDM660
+	case POWER_SUPPLY_PROP_RERUN_APSD:
+		val->intval = chg->float_rerun_apsd;
+		break;
+#endif
 #ifdef CONFIG_MACH_MI
 	case POWER_SUPPLY_PROP_TYPE_RECHECK:
 		rc = smblib_get_prop_type_recheck(chg, val);

duckyduckG/android_kernel_xiaomi_sdm845_419

  [    2.816999] get prop 155 is not supported in usb
Signed-off-by: duckyduckg <duckyduckg65@gmail.com>

drivers/power/supply/qcom/qpnp-smb2.c
@@ -719,6 +719,11 @@ static int smb2_usb_get_prop(struct power_supply *psy,
 	case POWER_SUPPLY_PROP_TYPE_RECHECK:
 		rc = smblib_get_prop_type_recheck(chg, val);
 		break;
+#ifdef CONFIG_MACH_XIAOMI
+	case POWER_SUPPLY_PROP_RERUN_APSD:
+		val->intval = chg->float_rerun_apsd;
+		break;
+#endif
 	default:
 		pr_err("get prop %d is not supported in usb\n", psp);
 		rc = -EINVAL;