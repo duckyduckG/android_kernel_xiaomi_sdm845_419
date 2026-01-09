595968810f8d81568a39874733194652d0721333
=?UTF-8?q?Noralf=20Tr=C3=B8nnes?= <noralf@tronnes.org>
2026-01-10 02:27:14 +0500

    BACKPORT: drm/driver: Add defaults for .gem_prime_export/import callbacks
    MIME-Version: 1.0
    Content-Type: text/plain; charset=UTF-8
    Content-Transfer-Encoding: 8bit
    
    The majority of drivers use drm_gem_prime_export() and
    drm_gem_prime_import() for these callbacks so let's make them the
    default.
    
    Signed-off-by: Noralf Trønnes <noralf@tronnes.org>
    Acked-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    Acked-by: Christian König <christian.koenig@amd.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20181110145647.17580-2-noralf@tronnes.org

--- include/drm/drm_drv.h
+++ include/drm/drm_drv.h
@@ -473,6 +473,8 @@ struct drm_driver {
 	 * @gem_prime_export:
 	 *
 	 * export GEM -> dmabuf
+	 *
+	 * This defaults to drm_gem_prime_export() if not set.
 	 */
 	struct dma_buf * (*gem_prime_export)(struct drm_device *dev,
 				struct drm_gem_object *obj, int flags);
@@ -480,6 +482,8 @@ struct drm_driver {
 	 * @gem_prime_import:
 	 *
 	 * import dmabuf -> GEM
+	 *
+	 * This defaults to drm_gem_prime_import() if not set.
 	 */
 	struct drm_gem_object * (*gem_prime_import)(struct drm_device *dev,
 				struct dma_buf *dma_buf);
