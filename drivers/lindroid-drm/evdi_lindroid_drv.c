551d5c53c979607704072518b41d986cb9c843ee
duckyduckG <duckyduckg65@gmail.com>
2026-01-11 00:05:29 +0500

    Revert "drivers: lindroid: enable gem_prime for k4.19"
    This reverts commit cfb2d92c24aba828bdd8fb60ccef1c7775329ff7.

--- drivers/lindroid-drm/evdi_lindroid_drv.c
+++ drivers/lindroid-drm/evdi_lindroid_drv.c
@@ -73,7 +73,7 @@ static struct drm_driver evdi_driver = {
 #if KERNEL_VERSION(5, 9, 0) <= LINUX_VERSION_CODE
 	.gem_create_object = NULL,
 #endif
-#if KERNEL_VERSION(4, 19, 0) <= LINUX_VERSION_CODE
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 11, 0)
 	.prime_handle_to_fd = drm_gem_prime_handle_to_fd,
 	.prime_fd_to_handle = drm_gem_prime_fd_to_handle,
 	.gem_prime_import_sg_table = evdi_prime_import_sg_table,
